0d7edbd881a23d4c53f1227d05a8e000ee78ef21
alialkadi <alialkady80@gmail.com>
2025-11-29 01:18:49 +0200

    updating link

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://coordinate-reveal-way-artificial.trycloudflare.com/api'.trim()
+  apiUrl: 'https://shoulder-buyer-advantage-thats.trycloudflare.com'.trim()
 };
+  
