e9efdbf80a048d82917df8c53fb747df49ac2473
alialkadi <72875869+alialkadi@users.noreply.github.com>
2025-11-29 19:41:50 +0200

    Update apiUrl to include '/api' endpoint

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://wealth-disable-strip-achievement.trycloudflare.com'.trim()
+  apiUrl: 'https://wealth-disable-strip-achievement.trycloudflare.com/api'.trim()
 };
   
