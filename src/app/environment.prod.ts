7c2176de69a5f714f689bb34f33e1ecf12f82155
alialkadi <alialkady80@gmail.com>
2025-11-29 19:18:31 +0200

    update02

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shoulder-buyer-advantage-thats.trycloudflare.com'.trim()
+  apiUrl: 'https://pie-purse-lincoln-speakers.trycloudflare.com'.trim()
 };
   
